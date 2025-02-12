6dd357ea708d06c69b374f89cebe5551f0a4caec
C4月M <romain.charollais87@gmail.com>
2025-02-12 11:51:39 +0100

    d
    d

---
+++ receiveTemp.php
@@ -0,0 +1,14 @@
+
+<?php
+$i;
+$temperature = array(10);
+ $fp=fopen("/home/pi/temp.txt","r");
+ if($fp==NULL) echo "Erreur ouverture";
+ else{
+    $valTemp = base_convert($hex, 16, 10);
+ echo $entier;
+ sleep(1);
+ fclose($fp);
+ }
+
+?> 
