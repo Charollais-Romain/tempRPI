4993d93fb5a70ecf3416b9b93f9c2d2cea6ecd12
C4月M <romain.charollais87@gmail.com>
2025-02-19 11:20:18 +0100

    g

--- receiveTemp.php
+++ receiveTemp.php
@@ -1,14 +1,25 @@
 <?php
 $temperature = array(10);
- $fp=fopen("/home/pi/tt.txt","r");
+ $fp=fopen("tt.txt","r");
  if($fp==NULL) echo "Erreur ouverture";
  else{
    $hex = fgets($fp);
    $valTemp = base_convert($hex, 16, 10);
    echo $valTemp;
-   echo "test";
    sleep(1);
    fclose($fp);
  }
-
 ?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <h1>Temperature</h1>
+   <h1><?php echo $valTemp; ?></h1>
+   <h1>°C</h1>
+</body>
+</html>
